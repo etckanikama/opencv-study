de653e5f52fad337cc17e9cc26e40d11ff94601e
etckanikama <funkybaby1230@ezweb.ne.jp>
2020-06-16 12:06:59 +0900

    putText関数の処理を追加しました

--- lines_on_image.py
+++ lines_on_image.py
@@ -16,6 +16,9 @@ try:
     # 座標=(50,50)から(200,50)まで青色255で線を引く
     cv2.line(img, (50,50), (200,50), (250,0,0))
 
+    # 座標=文字列の左下隅の座標、fontの種類=cv2.FONT_HERSHEY_SIMPLEX, フォントスケール、color(b,g,r),太さ　で文字をかく（putText）
+    cv2.putText(img, 'Hello OpenCV', (50,50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (50,60,80),2)
+
     cv2.imwrite('./tmp3/LineOnImage.jpg',img)
 
 except:
