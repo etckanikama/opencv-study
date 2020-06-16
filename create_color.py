a5116b32f6e151aa07cb8f7a3a297d9b65b5ded3
etckanikama <funkybaby1230@ezweb.ne.jp>
2020-06-16 16:37:17 +0900

    ファイル名を変更

--- create_color.py
+++ create_color.py
@@ -5,12 +5,12 @@ img = np.zeros((400,400,3), np.uint8)
 # すべてのpxに対して[:,:]色指定
 img[:,:] = [255,0,0]
 
-cv2.imwrite("./tmp/blueImage.jpg",img)
+cv2.imwrite("./tmp1/blueImage.jpg",img)
 # cv2.imshow('img1',img) conect to x serverの元　(linuxではめんどい？)
 
 img[:,:]=[0,255,0]
-cv2.imwrite('./tmp/greenImage.jpg',img)
+cv2.imwrite('./tmp1/greenImage.jpg',img)
 
 
 img[:,:] = [0,0,255]
-cv2.imwrite('./tmp/redImage.jpg',img)
+cv2.imwrite('./tmp1/redImage.jpg',img)
